44b1287e3b40a3c754f1dab10b56ca1886985a45
Hussain Ali <157008889+Hussain702@users.noreply.github.com>
2025-08-25 21:42:15 +0000

    changes

--- Week_05/Project/dags/Orchestration.py
+++ Week_05/Project/dags/Orchestration.py
@@ -6,7 +6,7 @@ from datetime import datetime
 # Define cluster config (for job runs)
 new_cluster = {
     'spark_version': '14.3.x-scala2.12',   # adjust to your cluster runtime
-    'node_type_id': 'Standard_DS3_v2',     # Azure node type example
+    'node_type_id': 'Standard_F4',     # Azure node type example
     'num_workers': 1
 }
 
@@ -25,10 +25,15 @@ transform_notebook_task = {
     }
 }
 
-
+load_notebook_task={
+    'new_cluster':new_cluster,
+    'notebook_task':{
+        'notebook_path':'/Workspace/Project/Load'
+    }
+}
 @dag(
     dag_id='orchestration_retail_project',
-    start_date=datetime(2025, 8, 22),  # fixed date (don’t use datetime.today())
+    start_date=datetime(2025, 8, 22),  
     schedule='@daily',
     catchup=False
 )
@@ -46,9 +51,13 @@ def RetailStore_etl_Pipeline():
         databricks_conn_id='databricks_default'
     )
 
-
+    load=DatabricksSubmitRunOperator(
+        task_id='load_task',
+        json=load_notebook_task,
+        databricks_conn_id='databricks_default'
+    )
     # Dependencies
-    extract >> transform 
+    extract >> transform >>load
 
 
 # Instantiate DAG
